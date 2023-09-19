e8fa48085647a9bfcfa9b892cd8410e9f9a11b4f
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2023-09-19 14:36:13 +0530

    q1 recursion

---
+++ basic_reccursion/print_string_5_times.js
@@ -0,0 +1,14 @@
+// print_string_5_times
+
+let string = "hello world";
+let count = 5
+function printString(string){
+    if(count == 0){
+        return
+    }
+    count--
+    console.log(string)
+    printString(string)
+}
+
+printString(string)
